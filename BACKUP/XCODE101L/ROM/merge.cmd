e2003947b4d5635e78f4228b1e19654b9af45fd3
Alex Meijer <asure007@gmail.com>
2025-04-29 01:42:13 +0200

    Cleanup part 1
    Removed Trivia contest leftovers
    Removed Rily and Miner leftovers
    Removed Sweepstakes code & messages

--- BACKUP/XCODE101L/ROM/merge.cmd
+++ BACKUP/XCODE101L/ROM/merge.cmd
@@ -4,5 +4,6 @@ copy /b mht100f8.0 + mht100fc.0  l1.03_maximum_hangtime_u54_l_version.u54
 copy /b mht100f8.1 + mht100fc.1  l1.03_maximum_hangtime_u63_l_version.u63
 del *.0 *.1
 rem uncomment and add orig roms to build flat merged bin for comparing.
+del rom_mine.bin
 c:\bin\far\srec_cat -o ROM_MINE.bin -binary l1.03_maximum_hangtime_u54_l_version.u54 -binary -unsplit 2 0 l1.03_maximum_hangtime_u63_l_version.u63 -binary -unsplit 2 1
 zip d:\mame\roms\nbamht.zip .\l1*.u*
