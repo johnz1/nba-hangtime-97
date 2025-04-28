c22f876d44a112e446ed938d78faff2a1cb7b063
Alex Meijer <asure007@gmail.com>
2025-04-28 15:15:49 +0200

    Update merge.cmd

--- BACKUP/XCODE101L/ROM/merge.cmd
+++ BACKUP/XCODE101L/ROM/merge.cmd
@@ -1,7 +1,7 @@
 @echo off
 del l*.u*
-copy /b mht100f8.0 + mht100fc.0  l1.0_maximum_hangtime_u54_l_version.u54
-copy /b mht100f8.1 + mht100fc.1  l1.0_maximum_hangtime_u63_l_version.u63
+copy /b mht100f8.0 + mht100fc.0  l1.03_maximum_hangtime_u54_l_version.u54
+copy /b mht100f8.1 + mht100fc.1  l1.03_maximum_hangtime_u63_l_version.u63
 del *.0 *.1
 rem uncomment and add orig roms to build flat merged bin for comparing.
 rem c:\bin\far\srec_cat -o ROM_MINE.bin -binary l1.0_maximum_hangtime_u54_l_version.u54 -binary -unsplit 2 0 l1.0_maximum_hangtime_u63_l_version.u63 -binary -unsplit 2 1
