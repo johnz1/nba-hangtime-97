bdc20f377ca4697a1e6ae7914a766f58ea057340
Alex Meijer <asure007@gmail.com>
2025-04-28 17:22:12 +0200

    Enable storm animation
    To see the MK3 animated storm, keep holding UP+Right after selecting teams and/or codes+court.

---
+++ BACKUP/XCODE101L/IMG/IMGT.cmd
@@ -0,0 +1,11 @@
+@echo off
+rem this CMD file will do everything and create bb3.bin.
+rem adjust paths as needed.
+c:\dosbox-x\dosbox-x.exe -fastlaunch -conf .\dosbox-x\dosbox-x.conf exit -c "c:\myht\backup\xcode1~3\img\imgt.bat"
+del misc2.old
+ren misc2.bin misc2.old
+dd if=misc2.irw of=misc2.bin bs=0x44 skip=1
+rem MAME debug commands to load these from the mame folder:
+rem loadr misc.bin,1200000,0,:gfxrom in mame debug
+copy misc2.bin D:\mame
+
