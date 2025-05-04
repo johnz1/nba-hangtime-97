38982c518779e7ec9aaee4a252445249d304faf6
Alex Meijer <asure007@gmail.com>
2025-05-05 00:25:34 +0200

    Fix automation. Add sounds.

--- BACKUP/XCODE101L/DCS/rebuild.cmd
+++ BACKUP/XCODE101L/DCS/rebuild.cmd
@@ -6,6 +6,9 @@ cd output
 del *.u*
 unzip ..\nbatest10.zip
 pause
+rem We're now overflowing into bigger romset, and the encoder names this file wrong!
+rem we need to update MAME for this!
+ren l1.0_nba_hangtime_u_6_music_spch.u5 *.u6
 zip d:\mame\roms\nbamht.zip .\l1.0_*.u*
 cd \mame
 mame.exe nbamht -debug
