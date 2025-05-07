72cd602ee426fff19a1597488554e1f2bbf77bb2
Alex Meijer <asure007@gmail.com>
2025-05-08 01:03:27 +0200

    Debug note, dosbox ram tricks
    Add Dosbox 0.74 rework.cmd using qemm vidram trick.
    Debug = 1 and more require this to compile. (use rework.cmd!)
    Updated debug docs.

--- BACKUP/XCODE101L/rework.cmd
+++ BACKUP/XCODE101L/rework.cmd
@@ -1,3 +1,6 @@
-"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -conf .\dosbox-x\dosbox-ECE.conf -noconsole
+"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -fastlaunch -conf .\dosbox-x\dosbox.conf -noconsole
+del mc*.*
+cd ROM
+merge.cmd
 
 	
