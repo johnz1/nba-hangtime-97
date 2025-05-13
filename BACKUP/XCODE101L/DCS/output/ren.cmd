7b332c8210b3d262e9610854c1e79520ef23077e
Alex Meijer <asure007@gmail.com>
2025-05-13 12:52:27 +0200

    Switch to 1993 DCS Subtype in script.
    Seems to fix audio!

---
+++ BACKUP/XCODE101L/DCS/output/ren.cmd
@@ -0,0 +1,7 @@
+del l1*.*
+ren u2.bin l1.0_nba_hangtime_u_2_music_spch.u2
+ren u3.bin l1.0_nba_hangtime_u_3_music_spch.u3
+ren u4.bin l1.0_nba_hangtime_u_4_music_spch.u4
+ren u5.bin l1.0_nba_hangtime_u_5_music_spch.u5
+ren u6.bin l1.0_nba_hangtime_u_6_music_spch.u6
+zip d:\mame\roms\nbamht.zip .\l1.0_*.u*
--- BACKUP/XCODE101L/DCS/rebuild.cmd
+++ BACKUP/XCODE101L/DCS/rebuild.cmd
@@ -4,12 +4,17 @@ DCSEncoder.exe -o nbatest10.zip --stream-dir=.\input --patch .\ORIGROM\nba.zip r
 rem DCSexplorer.exe -p nbatest10.zip >new_prog.txt
 mkdir output
 cd output
-del *.u*
+del u*.bin
+del l1*.*
 unzip ..\nbatest10.zip
 pause
 rem We're now overflowing into bigger romset, and the encoder names this file wrong!
 rem we need to update MAME for this!
-ren l1.0_nba_hangtime_u_6_music_spch.u5 *.u6
+ren u2.bin l1.0_nba_hangtime_u_2_music_spch.u2
+ren u3.bin l1.0_nba_hangtime_u_3_music_spch.u3
+ren u4.bin l1.0_nba_hangtime_u_4_music_spch.u4
+ren u5.bin l1.0_nba_hangtime_u_5_music_spch.u5
+ren u6.bin l1.0_nba_hangtime_u_6_music_spch.u6
 zip d:\mame\roms\nbamht.zip .\l1.0_*.u*
 cd \mame
 rem mame.exe nbamht -debug
